484196cf75fa383ccaa633eb49b45b880304dd9a
Emilse Arias <emilsearias@gmail.com>
2022-02-16 10:57:03 -0300

    se agrego la logica para crear productos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,3 +1,4 @@
+import Producto from "../models/producto";
 const productoCtrl = {};
 
 //agregamos la logica para obtener la lista de producto
@@ -6,9 +7,31 @@ productoCtrl.listarProductos = (req, res) =>{
     res.send('hola desde el backend')
 }
 
-productoCtrl.crearProducto = (req, res)=>{
- console.log(req.body)   
- res.send('voy a crear un producto')
+productoCtrl.crearProducto = async(req, res)=>{
+ try{
+     console.log(req.body) 
+     //validar  
+     //crear el producto en la base de datos
+     const productoNuevo = new Producto({
+        productName: req.body.productName,
+        price: req.body.price,
+        urlImg: req.body.urlImg,
+        category: req.body.category
+     })
+     //guardar el objeto nuevo en la BD
+     await productoNuevo.save();
+     //enviar respuesta
+     res.status(201).json({
+         mensaje: 'Producto correctamente creado'
+     })
+ }catch(error){
+     console.log(error);
+     //enviar un codigo de error
+     res.status(404).json({
+         mensaje: 'Error al intentar agregar un producto'
+     })
+ }
+
 }
 
 export default productoCtrl;
--- src/models/producto.js
+++ src/models/producto.js
@@ -1,1 +1,28 @@
 //modelamos el dato a guardar en la BD
+import mongoose, {Schema} from 'mongoose';
+
+const productoSchema = new Schema({
+    productName:{
+        type: String,
+        maxlength: 200,
+        unique: true,
+        required:true
+    },
+    price:{
+        type: Number,
+        required:true,
+        maxlength: 2000
+    },
+    urlImg:{
+        type: String,
+        required: true
+    },
+    category:{
+        type: String,
+        required: true
+    }
+});
+
+const Producto = mongoose.model('producto', productoSchema);
+
+export default Producto;
